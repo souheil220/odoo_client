75997e81cc5c00aa6dbaf9b09fdbf815aac921aa
souheil220 <souhil433@gmail.com>
2022-03-21 16:47:18 +0100

    created my index.html page

--- odoo_client/pages/views.py
+++ odoo_client/pages/views.py
@@ -1,17 +1,50 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+import re
+from .sql import connexion_ad2000, connexion_email
 
 # Create your views here.
 
 
 def index(request):
     if request.user.is_authenticated:
-        return redirect("clients/clients")
+        return redirect("clients/")
     return render(request, "pages/login.html")
 
 
 def loginPage(request):
-    return render(request, "pages/login.html")
+    if request.method == "POST":
+        username = request.POST.get("username").lower()
+        password = request.POST.get("password")
+
+        pass_django = "Azerty@22"
+        if not re.match(r"^[A-Za-z0-9\.\+-]+@[A-Za-z0-9\.-]+\.[a-zA-Z]*$", username):
+            email_util = "GROUPE-HASNAOUI\\" + username
+            connexion = connexion_ad2000(email_util, password)
+        else:
+            connexion = connexion_email(username, password)
+
+        print("connexion ", connexion)
+        if connexion == "deco":
+            messages.error(request, "Something went wrong")
+        else:
+            user = authenticate(
+                request, username=connexion["ad_2000"], password=pass_django
+            )
+            if user is None:
+                user = User.objects.create_user(
+                    connexion["ad_2000"], connexion["mail"], pass_django
+                )
+            print("user ", user)
+
+            if user is not None:
+                login(request, user)
+                return redirect("index")
+
+            else:
+                messages.error(request, "Accès non autorisé")
 
 
 def logoutUser(request):
